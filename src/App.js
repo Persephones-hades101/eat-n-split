3afb99aa778799dc0e38434357be033b65a19929
sudh4800@gmail.com <sudh4800@gmail.com>
2025-01-29 19:25:32 +0530

    added  key={selectedFriend.id} to FormSplitBill to fix the bug

--- src/App.js
+++ src/App.js
@@ -76,6 +76,7 @@ function App() {
         <FormSplitBill
           friend={selectedFriend}
           onBillSplit={handleBillSplit}
+          key={selectedFriend.id}
         />
       )}
     </div>
